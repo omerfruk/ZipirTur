b7dec6c8017df35fbb3554766f8785a4c3dde131
omerfruk <omer.fruk3547@gmail.com>
2020-03-04 01:51:08 +0300

    navbar color added

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   </head>
 
   <body>
-  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" id ="navbar">
     <a class="navbar-brand" href="#">Zıpır Tur</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
