26c9f592871f4753f9101cdab780944ad5750a32
omerfruk <omer.fruk3547@gmail.com>
2020-03-06 19:10:57 +0300

    yazıların boyutu küçüldğldğ

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
   <div class="row">
     <div class="col-md-12 m-3">
      <div ><img src="img/mardin/mardin1.jpg"class="border mr-5 img-thumbnail rounded float-left"width="300" height="300"/></div>
-     <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
+     <h5 class="text-center">Mardin</h5><h5 class="text-center">Dinlerin ve Dillerin Şehri</h5>
     <p class="text-center">Mardin ilinde tarihin çok eski dönemine uzanan dönemlere ait bulgular yer almaktadır.
        Artuklu Üniversitesi yerleşkesi içinde yapılan kazılarda ortaya çıkarılan taşlar, Paleolitik Çağ'a tarihlenmiştir <br><br>
        <a href="mardin.php" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
@@ -39,7 +39,7 @@
       </div>
       <div class="col-md-12 m-3">
         <img src="img/madrid/madrid1.jpg"class="border ml-3 img-thumbnail rounded float-right"width="300" height="300"/>
-         <h3 class="text-center">Madrid</h3><br><h4 class="text-center">Zengin Tarih</h4>
+         <h5 class="text-center">Madrid</h5><h5 class="text-center">Zengin Tarih</h5>
       <p class="text-center">  Zengin tarihi mirasının yanı sıra canlı bir kültür ve sanat merkezi olarak da önem taşır.
          Gelişmesi ve ülke ekonomisinde ağırlıklı bir rol kazanması yakın tarihlere rastlar.
          Metropolitan alanın yüzölçümü 1.020 km² olup nüfusu 7 milyonu aşkındır. <br><br>
@@ -48,7 +48,7 @@
         </div>
         <div class="col-md-12 m-3">
           <img src="img/antalya/antalya1.jpg"class="border mr-5 img-thumbnail rounded float-left"width="300" height="300"/>
-           <h3 class="text-center">Antalya</h3><br><h4 class="text-center">Turizimin Gözde Çiçeğii</h4>
+           <h5 class="text-center">Antalya</h5><h5 class="text-center">Turizimin Gözde Çiçeğii</h5>
           <p class="text-center">  Tamamı Akdeniz Bölgesi'nin batısında yer alır ve Antalya Körfezi'yle Batı Torosların arasında kurulmuştur.
              Yüzölçümü bakımından Türkiye'nin altıncı büyük ilidir.
              Güneyinde Akdeniz, batısında Muğla, kuzeyinde Burdur ve Isparta, kuzeydoğusunda Konya, doğusunda ise Karaman ve Mersin illeri vardır.<br><br>
@@ -57,7 +57,7 @@
         </div>
         <div class="col-md-12 m-3">
           <img src="img/hollywood/hollywood1.jpg"class="border ml-3 img-thumbnail rounded float-right" width="300" height="300"/>
-           <h3 class="text-center">HollyWood</h3><br><h4 class="text-center">Filmin Doğası</h4>
+           <h5 class="text-center">HollyWood</h5><h5 class="text-center">Filmin Doğası</h5>
           <p class="text-center"> ABD'de yapılan ilk filmler New York kenti civarında çekilmişti. 1900 yıllarına doğru Kaliforniya'da ilk filmler yapılmaya başlandı.
              Kaliforniya'nın tercih nedeni daha güzel bir havaya ve açık alanlara sahip olmasıydı. 
              Ayrıca bir diğer nedeni de ünlü buluşçu Thomas Edison'a sinema konusundaki patentlerinden dolayı ödeme yapmaktan 
@@ -67,63 +67,12 @@
         </div>
         <div class="col-md-12 m-3">
           <img src="img/paris/paris2.jpg"class="border mr-5 img-thumbnail rounded float-left" width="300" height="300"/>
-           <h3 class="text-center">Paris</h3><br><h4 class="text-center">Sanatın Merkezi</h4>
+           <h5 class="text-center">Paris</h5><h5 class="text-center">Sanatın Merkezi</h5>
         <p class="text-center">  Paris, Fransa'nın başkenti ve Île-de-France bölgesinin merkezidir. Sen Nehri'nin üzerine, Paris Havzası'nın ortasına kurulmuştur.
            Paris'te ikamet edenlere Parisien(ne) diye hitap edilir. Tüm dünyada anıtları, sanatsal ve kültürel yaşamı ile bilinen Paris, aynı zamanda dünya  <br><br>
          <a href="paris.php" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
         </p>
       </div>
-      <!--
-      <div class="col-md-12 m-3">
-        <img src="img/efes.jpg"class="border ml-3 img-thumbnail rounded float-right"width="300" height="300"/>
-         <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
-        <p class="text-center">  Ahmet Bey, küçük, ahşap, kırmızı boyalı, iki katlı bir evde otururdu. Erkenden kalkar,
-          "Haydi ya nasip" derdi ve evden çıkardı. Kimseler anlamazdı "Ya nasip" ne demekti?
-          O mahallede herkes gömlek giyerdi, Ahmet Bey bir de ceket diktirdi, mahalleli bundan
-          pek hoşlanmamış gibiydi. <br><br>
-         <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
-        </p>
-      </div>  <div class="col-md-12 m-3">
-        <img src="img/efes.jpg"class="border mr-5 img-thumbnail rounded float-left" width="300" height="300"/>
-         <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
-        <p class="text-center">  Ahmet Bey, küçük, ahşap, kırmızı boyalı, iki katlı bir evde otururdu. Erkenden kalkar,
-          "Haydi ya nasip" derdi ve evden çıkardı. Kimseler anlamazdı "Ya nasip" ne demekti?
-          O mahallede herkes gömlek giyerdi, Ahmet Bey bir de ceket diktirdi, mahalleli bundan
-          pek hoşlanmamış gibiydi. <br><br>
-         <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
-          </p>
-        </div>
-        <div class="col-md-12 m-3">
-          <img src="img/efes.jpg"class="border ml-3 img-thumbnail rounded float-right" width="300" height="300"/>
-           <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
-          <p class="text-center">  Ahmet Bey, küçük, ahşap, kırmızı boyalı, iki katlı bir evde otururdu. Erkenden kalkar,
-            "Haydi ya nasip" derdi ve evden çıkardı. Kimseler anlamazdı "Ya nasip" ne demekti?
-            O mahallede herkes gömlek giyerdi, Ahmet Bey bir de ceket diktirdi, mahalleli bundan
-            pek hoşlanmamış gibiydi. <br><br>
-           <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
-          </p>
-          </div>
-          <div class="col-md-12 m-3">
-            <img src="img/efes.jpg"class="border mr-5 img-thumbnail rounded float-left" width="300" height="300"/>
-             <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
-          <p class="text-center">  Ahmet Bey, küçük, ahşap, kırmızı boyalı, iki katlı bir evde otururdu. Erkenden kalkar,
-            "Haydi ya nasip" derdi ve evden çıkardı. Kimseler anlamazdı "Ya nasip" ne demekti?
-            O mahallede herkes gömlek giyerdi, Ahmet Bey bir de ceket diktirdi, mahalleli bundan
-            pek hoşlanmamış gibiydi. <br><br>
-           <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
-          </p>
-        </div>
-        <div class="col-md-12 m-3">
-          <img src="img/efes.jpg"class="border ml-3 img-thumbnail rounded float-right" width="300" height="300"/>
-           <h3 class="text-center">Mardin</h3><br><h4 class="text-center">Dinlerin ve Dillerin Şehri</h4>
-          <p class="text-center">  Ahmet Bey, küçük, ahşap, kırmızı boyalı, iki katlı bir evde otururdu. Erkenden kalkar,
-            "Haydi ya nasip" derdi ve evden çıkardı. Kimseler anlamazdı "Ya nasip" ne demekti?
-            O mahallede herkes gömlek giyerdi, Ahmet Bey bir de ceket diktirdi, mahalleli bundan
-            pek hoşlanmamış gibiydi. <br><br>
-           <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Daha Fazlası İçin...</a>
-          </p>
-        </div>
--->
   </div>
   </div>
 </div>
